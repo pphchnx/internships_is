5a29674e9cc8642268efddbe3704b1a8f1b59a20
testting <phuchana.wirancha@swu.ac.th>
2026-04-28 23:15:33 +0700

    Update modified files

--- hu_internships/includes/footer.php
+++ hu_internships/includes/footer.php
@@ -6,6 +6,9 @@
     <p style="margin-top:0.25rem; font-size:0.8rem;">
         <?= $t['footer_system'] ?>
     </p>
+    <p style="margin-top:1rem;">
+        <a href="https://miro.com/app/board/uXjVGgHyKhY=/?share_link_id=745674684806" target="_blank" style="color: var(--text-color); opacity: 0.7; text-decoration: underline; font-size: 0.85rem; transition: 0.2s;" onmouseover="this.style.opacity='1'; this.style.color='var(--primary-color)';" onmouseout="this.style.opacity='0.7'; this.style.color='var(--text-color)';">Site Map (Miro)</a>
+    </p>
 </div>
 
 <script src="<?= $base_url ?>/js/theme.js"></script>
